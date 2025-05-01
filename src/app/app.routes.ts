eb1ad65a22e00ba6b2720c76904ab479a3bf78e5
jhonc <calderonjhondairo@gmail.com>
2025-05-01 09:03:21 -0500

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login-page/login-page.component').then(
+        (m) => m.LoginPageComponent,
+      ),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register-page/register-page.component').then(
+        (m) => m.RegisterPageComponent,
+      ),
+  }
+];
